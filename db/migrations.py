f9d0878594c23dd41a690aefda62eb15c7e30c38
Robby Kanyur <robby@kanyur.com>
2024-10-07 21:15:55 -0500

    Set up sqlite database

---
+++ db/migrations.py
@@ -0,0 +1,36 @@
+import sqlite3
+conn = sqlite3.connect("./db/main.db")
+
+sql_create_races_table = """ CREATE TABLE IF NOT EXISTS races(
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  date TEXT NOT NULL,
+  crossresults_id TEXT NOT NULL
+)"""
+
+cursor = conn.cursor()
+cursor.execute(sql_create_races_table)
+
+races = [
+  ["Jackson Park", "2023-10-07", "11821"],
+  ["ABD Sunrise Park", "2023-10-22", "11934"],
+  ["Campton CX", "2023-10-29", "11999"],
+  ["Groundhog PSI", "2023-11-05", "12049"],
+  ["Wheeling Heritage Park", "2023-11-12", "12100"],
+  ["Quarry Cross", "2023-11-19", "12152"],
+  ["Montrose Beach CX", "2023-12-03", "12211"],
+  ["Glenwood Academy", "2024-09-29", "12471"]
+]
+
+def seed_races():
+  for race in races:
+    cursor.execute('''
+      INSERT INTO races (id, name, date, crossresults_id)
+      VALUES(?, ?, ?, ?)
+  ''', (None, race[0], race[1], race[2])) 
+
+seed_races()
+
+conn.commit()
+cursor.close()
+conn.close()
